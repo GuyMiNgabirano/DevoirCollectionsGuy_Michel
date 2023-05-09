bf2a0ace70c6ce3928aa8402b4b7bc04ae75f23e
GuyMiNgabirano <ngabiranog8@gmail.com>
2023-05-09 11:14:45 +0200

    afficher les elements de la liste

---
+++ main.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+
+   #Création d'une liste qui contient 10 éléments 
+
+   noms=['Ngabirano','Niyongabo','Igiraneza','Musobwa','Niyukuri','Bizindavyi','Nziza','Niyuhire','Irakoze','Ayinkamiye']
+
+   # 1. Affichage des éléments qui se trouve sur la liste 
+   print("----Afficher des éléments qui se trouve sur la premiére liste----")
+
+   for i in noms:
+       print(i)
